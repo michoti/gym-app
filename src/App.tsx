1e9541d34c7135dc635332a5fd7266f1d2f99180
shukeel <triggaillest@gmail.com>
2023-06-26 23:13:40 +0300

    navbar & enums

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import Navbar from "@/scenes/navbar";
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="app bg-gray-20">
+      <Navbar />
+    </div>
   )
 }
 
