d2f16521271df4cbe0d944be30b07173a12e92f9
shukeel <triggaillest@gmail.com>
2023-06-30 12:48:35 +0300

    modify benefits page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 // import OurClasses from "@/scenes/ourClasses";
-// import Benefits from "@/scenes/benefits";
+import Benefits from "@/scenes/benefits";
 // import ContactUs from "@/scenes/contactUs";
-// import Footer from "@/scenes/footer";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -35,10 +35,10 @@ function App({}: Props) {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
-      {/* <Benefits setSelectedPage={setSelectedPage} />
-      <OurClasses setSelectedPage={setSelectedPage} />
-      <ContactUs setSelectedPage={setSelectedPage} />
-      <Footer /> */}
+      <Benefits setSelectedPage={setSelectedPage} />
+      {/* <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} /> */}
+      <Footer />
     </div>
   );
 }
